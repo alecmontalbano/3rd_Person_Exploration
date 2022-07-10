65c42482add4769474952a910d4a0e0002c129b0
alec montalbano <alecmontalbano@alecs-MacBook-Pro.local>
2022-07-09 23:36:57 -0400

    Replaced hard local position setting with Physx rigidbody velocity and collisions

--- Assets/Scripts/MovingSphere.cs
+++ Assets/Scripts/MovingSphere.cs
@@ -10,12 +10,12 @@ public class MovingSphere : MonoBehaviour {
 
     Vector3 velocity;
 
+    Rigidbody body;
 
-    [SerializeField]
-    Rect allowedArea = new Rect(-5f, -5f, 10f, 10f);
-
-    [SerializeField, Range(0f, 1f)]
-    float bounciness = 0.5f;
+    void Awake()
+    {
+        body = GetComponent<Rigidbody>();    
+    }
 
     void Update () {
         Vector2 playerInput;
@@ -24,27 +24,12 @@ public class MovingSphere : MonoBehaviour {
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
         Vector3 desiredVelocity =
             new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        velocity = body.velocity;
         float maxSpeedChange = maxAcceleration * Time.deltaTime;
         velocity.x =
             Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
         velocity.z =
             Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
-        Vector3 displacement = velocity * Time.deltaTime;
-        Vector3 newPosition = transform.localPosition + displacement;
-        if (newPosition.x < allowedArea.xMin) {
-            newPosition.x = allowedArea.xMin;
-            velocity.x = -velocity.x * bounciness;
-        } else if (newPosition.x > allowedArea.xMax) {
-            newPosition.x = allowedArea.xMax;
-            velocity.x = -velocity.x * bounciness;
-        }
-        if (newPosition.z < allowedArea.yMin) {
-            newPosition.z = allowedArea.yMin;
-            velocity.z = -velocity.z * bounciness;
-        } else if (newPosition.z > allowedArea.yMax) {
-            newPosition.z = allowedArea.yMax;
-            velocity.z = -velocity.z * bounciness;
-        }
-        transform.localPosition = newPosition;
+        body.velocity = velocity;
     }
 }
